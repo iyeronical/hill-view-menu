006ad906ed64ef472fe17e14183236879d31eed7
Anant Iyer <iyeronical.ly@gmail.com>
2023-01-12 18:34:53 +0530

    MenuItems 90% done

--- client/src/components/MenuItems.jsx
+++ client/src/components/MenuItems.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function MenuItems({item}) {
-  return (
+    const [portion, setPortion] = useState('half');
+    const [quantity, setQuantity] = useState(1);
+    return (
     <div>
         <h3>{item.name}</h3>
         <img src={item.image} className="img-fluid" alt="" />
@@ -9,7 +11,7 @@ function MenuItems({item}) {
         <div className="flex-container">
             <div className="w-100">
                 <p>Portion</p>
-                <select>
+                <select value={portion} onChange={(e) => setPortion(e.target.value)}>
                     {item.size.map(s=>{
                         return <option value={s}>{s}</option>
                     })}
@@ -17,11 +19,20 @@ function MenuItems({item}) {
             </div>
             <div className="w-100">
                 <p>Quantity</p>
-                <select>
+                <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
                     {[...Array(10).keys()].map((x, i) => {
                         return <option value={i+1}>{i+1}</option>
                     })}
                 </select>
+            </div>
+        </div>
+        <div className="flex-container">
+            <div className="">
+                <p>Price : {item.price[0][portion] * quantity}</p>
+                
+            </div>
+            <div className="">
+
             </div>
         </div>
     </div>
