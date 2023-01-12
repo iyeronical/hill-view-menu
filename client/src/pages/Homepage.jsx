e81d23961dc8187167288e5c13e487694c493172
Anant Iyer <iyeronical.ly@gmail.com>
2023-01-12 18:45:33 +0530

    minor changes

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -4,13 +4,13 @@ import TopNavbar from '../components/TopNavbar'
 
 import menu from "../data.js"
 
-function Homepage() {
+function Homepage() { 
   return (
     <>
     <TopNavbar />
     <div className="row">
         {
-            menu.map(item=>{
+            menu.map(item => {
                 return  <div className="col-md-3">
                         <div>
                             <MenuItems item = {item} />
